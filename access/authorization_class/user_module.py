99699eacf59a06c4042959bb13230f58a025c72c
Shymaa123456 <shymaa.helal@hyperone.com.eg>
2020-11-22 16:46:49 +0200

    upload the code

--- access/authorization_class/user_module.py
+++ access/authorization_class/user_module.py
@@ -16,12 +16,11 @@ class CL_userModule(object):
     def loadPrivilages(self):
         self.conn = db1.connect()
         mycursor = self.conn.cursor()
-        sql_select_query = "select r.ROLE_ID , f.FORM_DESC  ,a.ACTION_ID ,fi.ITEM_ID " \
+        sql_select_query = "select r.ROLE_ID , f.FORM_DESC ,f.FORM_ID ,a.ACTION_ID ,pi.ITEM_ID " \
                            "from SYS_PRIVILEGE p " \
-                           "left outer join SYS_FORM_ITEM fi on p.FORM_ID = fi.FORM_ID  " \
                            "inner join SYS_FORM f on  p.FORM_ID= f.FORM_ID " \
                            "inner join SYS_PRINT_EXPORT a on p.ACTION_ID = a.ACTION_ID " \
-                           "left outer join SYS_PRIVILEG_ITEM pi on p.PRIV_ID= pi.PRIV_ID  and p.FORM_ID=pi.FORM_ID and pi.ITEM_ID = fi.ITEM_ID  " \
+                           "left outer join SYS_PRIVILEG_ITEM pi on p.PRIV_ID= pi.PRIV_ID  and p.FORM_ID=pi.FORM_ID  " \
                            "inner join SYS_USER_ROLE  ur on p.ROLE_ID = ur.ROLE_ID " \
                            "inner join SYS_ROLE r on r.ROLE_ID = p.ROLE_ID " \
                            "inner join SYS_USER u ON u.USER_ID = ur.USER_ID" \
@@ -29,10 +28,10 @@ class CL_userModule(object):
         #print(sql_select_query)
         x = (CL_userModule.user_name,)
 
-        # print(sql_select_query)
+        print(sql_select_query)
         mycursor.execute( sql_select_query, x )
 
         records = mycursor.fetchall()
         #print(records)
         CL_userModule.myList = records
-        #print(CL_userModule.myList)
+        print(CL_userModule.myList)

8357c6ae89dfb091472a303c74ab394387c01831
mohamedfaw <mohamed.fawzy@hyperone.com.eg>
2020-11-26 16:44:47 +0200

    fawzy 26/11/2020-1

--- access/promotion_class/Promotion_Add.py
+++ access/promotion_class/Promotion_Add.py
@@ -3,6 +3,53 @@ from PyQt5 import QtWidgets, QtCore
 from PyQt5.uic import loadUi
 from data_connection.h1pos import db1
 
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys
+
+
+# new check-able combo box- in test
+class CheckableComboBox(QComboBox):
+
+    # constructor
+    def __init__(self, parent=None):
+        super(CheckableComboBox, self).__init__(parent)
+        self.view().pressed.connect(self.handleItemPressed)
+        self.setModel(QStandardItemModel(self))
+
+    count = 0
+
+    # action called when item get checked
+    def do_action(self):
+
+        CL_create_promotion.label.setText("Checked number : " + str(self.count))
+
+        # when any item get pressed
+
+    def handleItemPressed(self, index):
+
+        # getting the item
+        item = self.model().itemFromIndex(index)
+
+        # checking if item is checked
+        if item.checkState() == Qt.Checked:
+
+            # making it unchecked
+            item.setCheckState(Qt.Unchecked)
+
+            # if not checked
+        else:
+            # making the item checked
+            item.setCheckState(Qt.Checked)
+
+            self.count += 1
+
+            # call the action
+            self.do_action()
+
+
 class CL_create_promotion(QtWidgets.QDialog):
     switch_window = QtCore.pyqtSignal()
     dirname = ''
@@ -16,9 +63,13 @@ class CL_create_promotion(QtWidgets.QDialog):
 
 
 
+
     def FN_LOAD_CREATE_PROM(self):
         filename = self.dirname + '/Promotion_create.ui'
         loadUi(filename, self)
+
+
+
 
         self.FN_GET_Company()
         self.FN_GET_Branch()
@@ -57,6 +108,9 @@ class CL_create_promotion(QtWidgets.QDialog):
         for row in records:
             self.Qcombo_cust_group.addItems( row )
         mycursor.close()
+
+        #  self.Qcombo_cust_group = CheckableComboBox(self)
+
     def FN_GET_MAGAZINE(self):
         self.conn = db1.connect()
         mycursor = self.conn.cursor()

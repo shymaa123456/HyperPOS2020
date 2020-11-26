9b195c21819e0d74bfdb432069a249f5634cfc4d
mohamedfaw <mohamed.fawzy@hyperone.com.eg>
2020-11-26 14:10:29 +0200

    fawzy 26/11/2020

--- access/promotion_class/Promotion_Add.py
+++ access/promotion_class/Promotion_Add.py
@@ -14,13 +14,26 @@ class CL_create_promotion(QtWidgets.QDialog):
         self.dirname = mod_path.__str__() + '/presentation/promotion_ui'
         self.conn = db1.connect()
 
+
+
     def FN_LOAD_CREATE_PROM(self):
         filename = self.dirname + '/Promotion_create.ui'
         loadUi(filename, self)
 
+        self.FN_GET_Company()
+
+
         # self.BTN_createUser.clicked.connect(self.FN_CREATE_USER)
 
-
+    def FN_GET_Company(self):
+        self.conn = db1.connect()
+        mycursor = self.conn.cursor()
+        mycursor.execute("SELECT COMPANY_DESC FROM COMPANY")
+        records = mycursor.fetchall()
+        print(records)
+        for row in records:
+            self.Qcombo_company.addItems( row )
+        mycursor.close()
 
 
 

b85800ac9a37a5b14902cac873b3d7918502ae5b
Shymaa123456 <shymaa.helal@hyperone.com.eg>
2021-02-10 12:04:46 +0200

    comments of 3-2-2021

--- access/loyalty_class/test.py
+++ access/loyalty_class/test.py
@@ -1,38 +1,22 @@
-import sys
-from PyQt5 import QtWidgets, QtCore
-
-class Window(QtWidgets.QMainWindow):
-
-    def __init__(self):
-        super(Window, self).__init__()
-        self.setGeometry(50,50,500,500)
-        self.setWindowTitle('PyQt Tuts')
-        self.table()
+# Python program to check if  
+# given mobile number is valid 
+import re
 
 
-    def table(self):
+def isValid(s):
+    # 1) Begins with 0 or 91
+    # 2) Then contains 7 or 8 or 9. 
+    # 3) Then contains 9 digits 
+    Pattern = re.compile("(0)?[0-9]")
+    return Pattern.match(s)
 
-        comboBox = QtWidgets.QComboBox()
 
-        self.tableWidget = QtWidgets.QTableWidget()
-        self.tableWidget.setGeometry(QtCore.QRect(220, 100, 411, 392))
-        self.tableWidget.setColumnCount(2)
-        self.tableWidget.setRowCount(5)
-        self.tableWidget.show()
+# Driver Code
+s = "222ss2222"
+#s='asdfsadsd23432423'
+if (isValid(s)):
+    print("Valid Number")
+else:
+    print("Invalid Number")
 
-        attr = ['one', 'two', 'three', 'four', 'five']
-        i = 0
-        for j in attr:
-            self.tableWidget.setItem(i, 0, QtWidgets.QTableWidgetItem(j))
-            comboBox = QtWidgets.QComboBox()
-            comboBox.addItem('110')
-            comboBox.addItem('210')
-            comboBox.addItem('310')
-            self.tableWidget.setCellWidget(i, 1, comboBox)
-            i += 1
-def run():
-    app = QtWidgets.QApplication(sys.argv)
-    w = Window()
-    sys.exit(app.exec_())
-
-run()
+    # This code is contributed by rishabh_jain
